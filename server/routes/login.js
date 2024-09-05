4b8630874f9f6b918ea00422d95c4fe374d4ba36
Arthur Duarte <tuiduarte@gmail.com>
2024-09-05 17:33:27 -0400

    feat: improve login system

--- server/routes/login.js
+++ server/routes/login.js
@@ -8,20 +8,15 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', async (req, res, next) => {
+    const { username, password } = req.body;
     try {
-        const user = await User.findOne({ username: req.body.username });
-        if (user) {
-            if (user.password === req.body.password){
-                console.log("Logged in");
-                return res.status(200).json({ user });
-            } else {
-                console.log("Incorrect password")
-                return res.status(401).json({ message: "Incorrect password" });
-            }
-        } else {
-            console.log("User not found");
-            return res.status(404).json({ message: "User not found" });
+        const user = await User.findOne({ username, password });
+        if (!user) {
+            return res.status(401).json({ message: "Invalid username or password" });
         }
+
+        return res.status(200).json({ message: "Login successful", user });
+        
     } catch (err) {
         console.error(err);
         return res.status(500).json({ message: "Internal server error" });
