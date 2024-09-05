8b16874c1aff714b917683e1275d2d2adc9df241
Arthur Duarte <tuiduarte@gmail.com>
2024-09-05 17:58:33 -0400

    feat: add session logout

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const loginRouter = require('./routes/login');
 const registerRouter = require('./routes/register');
+const logoutRouter = require('./routes/logout');
 
 const app = express();
 
@@ -22,10 +23,15 @@ app.use(session({
     saveUninitialized: false,
     name: 'session'
 }))
+app.use((req, res, next) => {
+    res.locals.user = req.session.user
+    next()
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
+app.use('/logout', logoutRouter);
 
 module.exports = app;
