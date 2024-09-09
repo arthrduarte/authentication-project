120d543bbcf5da699401b8357344b755920ae42d
Arthur Duarte <tuiduarte@gmail.com>
2024-09-09 12:04:12 -0400

    feat: add register page

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,12 +1,55 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 export default function Register() {
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
+    const navigate = useNavigate()
+
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+
+        const response = await fetch('http://localhost:3000/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password, email }),
+            credentials: 'include'
+        });
+
+        const data = await response.json();
+        if (response.ok) {
+            localStorage.setItem('user', JSON.stringify(data.user));
+            navigate('/')
+        } else {
+            setErrorMessage(data.message)
+        }
+    }
 
     return (
-        <>
+        <div>
+
             <a href="/">Go back</a>
             <h1 className='text-center font-bold text-[2rem]'>Register</h1>
-        </>
+            <p className='text-center font-bold text-[1rem]'>{errorMessage}</p>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                </div>
+                <button type="submit">Register</button>
+            </form>
+        </div>
     )
 }
