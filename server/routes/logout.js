390cc3f54bb22733b326a05f9309efb92841efa7
Arthur Duarte <tuiduarte@gmail.com>
2024-09-10 16:45:19 -0400

    fix: fix logout error when starting the application

--- server/routes/logout.js
+++ server/routes/logout.js
@@ -2,19 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { isAuthenticated } = require('../middlewares')
 
-router.post('/', (req, res) => {
-    if (req.session.user) {
-        req.session.destroy((err) => {
-            if (err) {
-                return res.status(500).json({ message: 'Failed to log out' });
-            }
-            console.log('Session destroyed and cookies cleared');
-            res.clearCookie('session'); 
-            return res.status(200).json({ message: 'Logout successful' });
-        });
-    } else {
-        res.status(404).json({ message: 'You are not logged in' })
-    }
+router.post('/', isAuthenticated, (req, res) => {
+    req.session.destroy()
+    console.log('Session destroyed and cookies cleared');
+    res.clearCookie('session');
+    return res.status(200).json({ message: 'Logout successful' });
 })
 
 module.exports = router
