a59edfc7f84dc81a1f8ce7395b67a673f68e36a1
Arthur Duarte <tuiduarte@gmail.com>
2024-09-11 12:54:25 -0400

    fix: update cors origin

--- server/app.js
+++ server/app.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
     next()
 })
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://authentication-project-7mz7.onrender.com',
     credentials: true
 }))
 
